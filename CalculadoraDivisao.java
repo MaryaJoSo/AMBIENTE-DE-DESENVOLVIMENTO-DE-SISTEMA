ae173a630fd3a2f9bc88d812eb38ca3bef0f9a09
lhlimasan <lhlimasan@gmail.com>
2023-11-29 23:13:01 -0300

    criacao de repositorio

--- CalculadoraDivisao.java
+++ CalculadoraDivisao.java
@@ -6,4 +6,12 @@ public class CalculadoraDivisao {
     public double dividir(double a, double b, double c) {
         return a / (b * c);
     }
+
+    public static void main(String[] args) {
+        CalculadoraDivisao calculadora = new CalculadoraDivisao();
+        double resultadoDivisao = calculadora.dividir(10.0, 2.0);
+        double resultadoDivisaoComposto = calculadora.dividir(20.0, 4.0, 2.0);
+        System.out.println("Resultado da divisão: " + resultadoDivisao);
+        System.out.println("Resultado da divisão composta: " + resultadoDivisaoComposto);
+    }
 }
