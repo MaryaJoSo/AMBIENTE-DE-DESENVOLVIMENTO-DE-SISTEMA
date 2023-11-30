f6371571bb8099c470bee0c9827b088c8921e47e
Maria Sousa <mariajsousa@edu.unifor.br>
2023-11-29 23:08:10 -0300

    inital commit

---
+++ CalculadoraDivisao.java
@@ -0,0 +1,9 @@
+public class CalculadoraDivisao {
+    public double dividir(double a, double b) {
+        return a / b;
+    }
+
+    public double dividir(double a, double b, double c) {
+        return a / (b * c);
+    }
+}
